b6c97d186c4cc44c5f89e9bce28216ca671c5102
Egorov Mikhail <misha2003200@gmail.com>
2024-03-29 17:57:03 +0300

    Initial Spring Boot

---
+++ src/test/java/ru/egorov/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egorov.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
