08e578c2103640c69c4e890be0088d94c6b0d769
Egorov Mikhail <misha2003200@gmail.com>
2024-03-29 21:22:20 +0300

    Добавить интерфейс для сервиса получения стоимости ценных бумаг

--- src/main/java/ru/egorov/bankaccount/datacurrencypairs/service/DataCurrencyService.java
+++ src/main/java/ru/egorov/bankaccount/datacurrencypairs/service/DataCurrencyService.java
@@ -1,52 +1,22 @@
 package ru.egorov.bankaccount.datacurrencypairs.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import ru.egorov.bankaccount.datacurrencypairs.dto.CurrencyDto;
-import ru.egorov.bankaccount.datacurrencypairs.entity.CurrencyCache;
-import ru.egorov.bankaccount.datacurrencypairs.repository.CurrencyCacheRepository;
-
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Optional;
 
-@Slf4j
-@Service
-public class DataCurrencyService {
+public interface DataCurrencyService {
+    /**
+     * Получение стоимости ценной бумаги
+     *
+     * @param value обозначение ценной бумаги
+     * @return стоимость ценной бумаги
+     */
+    BigDecimal getCurrentValue(String value);
 
-    @Value("${twelvedata.apikey}")
-    private String apiKey;
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    private final CurrencyCacheRepository cacheRepository;
-
-    @Autowired
-    public DataCurrencyService(CurrencyCacheRepository cacheRepository) {
-        this.cacheRepository = cacheRepository;
-    }
-
-    public BigDecimal getCurrentValue(String value) {
-        Optional<CurrencyCache> currencyCache = cacheRepository.findById(value);
-        if (currencyCache.isPresent() && currencyCache.get().getDate().equals(LocalDate.now())) {
-            log.debug("Получение валюты из кэша");
-            return currencyCache.get().getValue();
-        }
-
-        log.debug("Получение валюты из API");
-        CurrencyDto currencyDto = restTemplate.getForObject(String.format(
-                "https://api.twelvedata.com/eod?symbol=%s&apikey=%s",
-                value, apiKey
-        ), CurrencyDto.class);
-        log.debug("Кэшируем полученные данные в БД");
-        cacheRepository.save(new CurrencyCache(value, LocalDate.now(), currencyDto.getValue()));
-        return currencyDto.getValue();
-    }
-
-    public BigDecimal getCurrentValuePairs(String value1, String value2) {
-        return getCurrentValue(value1 + "/" + value2);
-    }
-
+    /**
+     * Получение стоимости валютной пары
+     *
+     * @param value1 первая валюта
+     * @param value2 вторая валюта
+     * @return стоимость валютной пары
+     */
+    BigDecimal getCurrentValuePairs(String value1, String value2);
 }
